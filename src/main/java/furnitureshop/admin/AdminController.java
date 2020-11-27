4ee8bdb744a1882cb28a07cd3e1a4b4fd6b1794f
sjaster <sebastian.jaster@mailbox.tu-dresden.de>
2020-11-27 10:11:01 +0100

    add javadoc to admincontroller #41 and minor improvements on routing

--- src/main/java/furnitureshop/admin/AdminController.java
+++ src/main/java/furnitureshop/admin/AdminController.java
@@ -1,19 +1,48 @@
 package furnitureshop.admin;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+/**
+ * Spring MVC Controller to manage the login and admin routes.
+ * Admin route is secured by {@link PreAuthorize} clause.
+ *
+ * @author Sebastian Jaster
+ */
 @Controller
 public class AdminController {
 
+	/**
+	 * Creates a new {@link AdminController}
+	 */
 	public AdminController() {}
 
+	/**
+	 * Gets the login view except when user is already authenticated
+	 * then it redirects to {@code getAdminInterface()}
+	 *
+	 * @param authentication
+	 * @return login view or redirect to admin overview when logged in
+	 */
 	@GetMapping("/login")
-	String getLogin() {
+	String getLogin(Authentication authentication) {
+		if(authentication != null) {
+			if (authentication.isAuthenticated()) {
+				return "redirect:/admin/overview";
+			}
+		}
 		return "login";
 	}
 
+	/**
+	 * Returns the admin view only for authenticated employees
+	 * hence the usage of {@code PreAuthorize}
+	 *
+	 * @return Admin view
+	 */
 	@PreAuthorize("hasRole('EMPLOYEE')")
 	@GetMapping("/admin/overview")
 	String getAdminInterface() {
