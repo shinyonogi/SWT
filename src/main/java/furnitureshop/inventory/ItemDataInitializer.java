1b604578097a2e71522fa09f605a71a02821c8e1
sjaster <sebastian.jaster@mailbox.tu-dresden.de>
2020-12-03 11:10:09 +0100

    add custom suppliers and less items in setup in itemservice test #51

--- src/main/java/furnitureshop/inventory/ItemDataInitializer.java
+++ src/main/java/furnitureshop/inventory/ItemDataInitializer.java
@@ -9,6 +9,7 @@ import org.salespointframework.core.DataInitializer;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -35,46 +36,39 @@ public class ItemDataInitializer implements DataInitializer {
 
 		Iterator<Supplier> iterator = supplierRepository.findAll().iterator();
 		Supplier supplier = iterator.next();
+		List<Item> items = new ArrayList<>();
 
-		Piece Stuhl1 = new Piece(1, "Stuhl 1", Money.of(59.99, Currencies.EURO), "/resources/img/chair_2.jpg", "schwarz",
+		Piece stuhl1 = new Piece(1, "Stuhl 1", Money.of(59.99, Currencies.EURO), "/resources/img/chair_2.jpg", "schwarz",
 				"Stuhl 1 in schwarz.", supplier, 5, Category.CHAIR);
 
+		items.add(stuhl1);
 		supplier = iterator.next();
 
-		Piece Sofa1_green = new Piece(2, "Sofa 1", Money.of(259.99, Currencies.EURO), "/resources/img/sofa_2_green.jpg", "grün",
-				"Sofa 1 in grün.", supplier, 50, Category.COUCH);
+		items.add(new Piece(2, "Sofa 1", Money.of(259.99, Currencies.EURO), "/resources/img/sofa_2_green.jpg", "grün",
+				"Sofa 1 in grün.", supplier, 50, Category.COUCH));
 
-		Piece Sofa1_red = new Piece(2, "Sofa 1", Money.of(259.99, Currencies.EURO), "/resources/img/sofa_2_red.jpg", "rot",
-				"Sofa 1 in rot.", supplier, 50, Category.COUCH);
+		items.add(new Piece(2, "Sofa 1", Money.of(259.99, Currencies.EURO), "/resources/img/sofa_2_red.jpg", "rot",
+				"Sofa 1 in rot.", supplier, 50, Category.COUCH));
 
-		Piece Sofa1_white = new Piece(2, "Sofa 1", Money.of(259.99, Currencies.EURO), "/resources/img/sofa_2_white.jpg", "weiß",
-				"Sofa 1 in weiß.", supplier, 80, Category.COUCH);
+		items.add(new Piece(2, "Sofa 1", Money.of(259.99, Currencies.EURO), "/resources/img/sofa_2_white.jpg", "weiß",
+				"Sofa 1 in weiß.", supplier, 80, Category.COUCH));
 
-		Piece Sofa1_grey = new Piece(2, "Sofa 1", Money.of(259.99, Currencies.EURO), "/resources/img/sofa_2_grey.jpg", "grau",
+		Piece sofa1_grey = new Piece(2, "Sofa 1", Money.of(259.99, Currencies.EURO), "/resources/img/sofa_2_grey.jpg", "grau",
 				"Sofa 1 in grau.", supplier, 80, Category.COUCH);
 
+		items.add(sofa1_grey);
 		supplier = iterator.next();
 
-		Piece Tisch1 = new Piece(3, "Tisch 1", Money.of(89.99, Currencies.EURO), "/resources/img/table_2.jpg", "weiß",
-				"Tisch 1 in weiß.", supplier, 30, Category.TABLE);
-
-		List<Item> Set1_items = new ArrayList<>();
-		Set1_items.add(Stuhl1);
-		Set1_items.add(Sofa1_grey);
+		items.add(new Piece(3, "Tisch 1", Money.of(89.99, Currencies.EURO), "/resources/img/table_2.jpg", "weiß",
+				"Tisch 1 in weiß.", supplier, 30, Category.TABLE));
 
 		Supplier setSupplier = new Supplier("Set Supplier", 0.05);
 		supplierRepository.save(setSupplier);
 
-		Set Set1 = new Set(4, "Set 1", Money.of(299.99, Currencies.EURO), "/resources/img/set_1.jpg", "black",
-				"Set bestehend aus Sofa 1 und Stuhl 1.", setSupplier, Category.SET, Set1_items);
+		items.add(new Set(4, "Set 1", Money.of(299.99, Currencies.EURO), "/resources/img/set_1.jpg", "black",
+				"Set bestehend aus Sofa 1 und Stuhl 1.", setSupplier, Category.SET, Arrays.asList(stuhl1, sofa1_grey)));
 
-		itemCatalog.save(Stuhl1);
-		itemCatalog.save(Sofa1_green);
-		itemCatalog.save(Sofa1_grey);
-		itemCatalog.save(Sofa1_red);
-		itemCatalog.save(Sofa1_white);
-		itemCatalog.save(Tisch1);
-		itemCatalog.save(Set1);
+		itemCatalog.saveAll(items);
 	}
 
 }
--- src/test/java/furnitureshop/inventory/ItemServiceTests.java
+++ src/test/java/furnitureshop/inventory/ItemServiceTests.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -31,61 +32,37 @@ public class ItemServiceTests {
 	@Autowired
 	ItemService itemservice;
 
-	ProductIdentifier id;
-
 	@BeforeEach
 	void setUp() {
 		itemCatalog.deleteAll();
-		Iterator<Supplier> iterator = supplierRepository.findAll().iterator();
-		Supplier supplier = iterator.next();
+		supplierRepository.deleteAll();
 
-		Piece Stuhl1 = new Piece(1, "Stuhl 1", Money.of(59.99, Currencies.EURO), "/resources/img/chair_2.jpg", "schwarz",
+		Supplier supplier = new Supplier("Supplier 1", 0.15);
+		Supplier setSupplier = new Supplier("Set Supplier", 0.05);
+		List<Supplier> suppliers = new ArrayList<>(Arrays.asList(supplier, setSupplier));
+		List<Item> items = new ArrayList<>();
+
+		Piece stuhl1 = new Piece(1, "Stuhl 1", Money.of(59.99, Currencies.EURO), "/resources/img/chair_2.jpg", "schwarz",
 				"Stuhl 1 in schwarz.", supplier, 5, Category.CHAIR);
-
-		supplier = iterator.next();
-
-		Piece Sofa1_green = new Piece(2, "Sofa 1", Money.of(259.99, Currencies.EURO), "/resources/img/sofa_2_green.jpg", "grün",
-				"Sofa 1 in grün.", supplier, 50, Category.COUCH);
-
-		Piece Sofa1_red = new Piece(2, "Sofa 1", Money.of(259.99, Currencies.EURO), "/resources/img/sofa_2_red.jpg", "rot",
-				"Sofa 1 in rot.", supplier, 50, Category.COUCH);
-
-		Piece Sofa1_white = new Piece(2, "Sofa 1", Money.of(259.99, Currencies.EURO), "/resources/img/sofa_2_white.jpg", "weiß",
-				"Sofa 1 in weiß.", supplier, 80, Category.COUCH);
-
-		Piece Sofa1_grey = new Piece(2, "Sofa 1", Money.of(259.99, Currencies.EURO), "/resources/img/sofa_2_grey.jpg", "grau",
+		items.add(new Piece(2, "Sofa 1", Money.of(259.99, Currencies.EURO), "/resources/img/sofa_2_green.jpg", "grün",
+				"Sofa 1 in grün.", supplier, 50, Category.COUCH));
+		Piece sofa1_grey = new Piece(2, "Sofa 1", Money.of(259.99, Currencies.EURO), "/resources/img/sofa_2_grey.jpg", "grau",
 				"Sofa 1 in grau.", supplier, 80, Category.COUCH);
 
-		supplier = iterator.next();
+		items.add(stuhl1);
+		items.add(sofa1_grey);
+		items.add(new Piece(3, "Tisch 1", Money.of(89.99, Currencies.EURO), "/resources/img/table_2.jpg", "weiß",
+				"Tisch 1 in weiß.", supplier, 30, Category.TABLE));
+		items.add(new Set(4, "Set 1", Money.of(299.99, Currencies.EURO), "/resources/img/set_1.jpg", "black",
+				"Set bestehend aus Sofa 1 und Stuhl 1.", setSupplier, Category.SET, Arrays.asList(stuhl1, sofa1_grey)));
 
-		Piece Tisch1 = new Piece(3, "Tisch 1", Money.of(89.99, Currencies.EURO), "/resources/img/table_2.jpg", "weiß",
-				"Tisch 1 in weiß.", supplier, 30, Category.TABLE);
-
-		List<Item> Set1_items = new ArrayList<>();
-		Set1_items.add(Stuhl1);
-		Set1_items.add(Sofa1_grey);
-
-		Supplier setSupplier = new Supplier("Set Supplier", 0.05);
-		supplierRepository.save(setSupplier);
-
-		Set Set1 = new Set(4, "Set 1", Money.of(299.99, Currencies.EURO), "/resources/img/set_1.jpg", "black",
-				"Set bestehend aus Sofa 1 und Stuhl 1.", setSupplier, Category.SET, Set1_items);
-
-		id = Stuhl1.getId();
-
-		itemCatalog.save(Stuhl1);
-		itemCatalog.save(Sofa1_green);
-		itemCatalog.save(Sofa1_grey);
-		itemCatalog.save(Sofa1_red);
-		itemCatalog.save(Sofa1_white);
-		itemCatalog.save(Tisch1);
-		itemCatalog.save(Set1);
-
+		supplierRepository.saveAll(suppliers);
+		itemCatalog.saveAll(items);
 	}
 
 	@Test
 	void findAllItemsBySizeTest() {
-		assertEquals(itemservice.findAll().stream().count(), 7, "ItemService.findAll() should find all items!/Number of items in catalog is wrong!");
+		assertEquals(itemservice.findAll().stream().count(), 5, "ItemService.findAll() should find all items!/Number of items in catalog is wrong!");
 	}
 
 	/*
@@ -98,7 +75,7 @@ public class ItemServiceTests {
 	@Test
 	void findAllByGroupIdBySizeTest() {
 		assertEquals(itemservice.findAllByGroupId(1).stream().count(), 1, "ItemService.findAll() should find all items by groupIDs!/Number of items in catalog is wrong!");
-		assertEquals(itemservice.findAllByGroupId(2).stream().count(), 4, "ItemService.findAll() should find all items by groupIDs!/Number of items in catalog is wrong!");
+		assertEquals(itemservice.findAllByGroupId(2).stream().count(), 2, "ItemService.findAll() should find all items by groupIDs!/Number of items in catalog is wrong!");
 		assertEquals(itemservice.findAllByGroupId(3).stream().count(), 1, "ItemService.findAll() should find all items by groupIDs!/Number of items in catalog is wrong!");
 		assertEquals(itemservice.findAllByGroupId(4).stream().count(), 1, "ItemService.findAll() should find all items by groupIDs!/Number of items in catalog is wrong!");
 
@@ -106,7 +83,7 @@ public class ItemServiceTests {
 
 	@Test
 	void findAllByCategoryBySizeTest() {
-		assertEquals(itemservice.findAllByCategory(Category.COUCH).stream().count(), 4, "ItemService.findAll() should find all items by categories!/Number of items in catalog is wrong!");
+		assertEquals(itemservice.findAllByCategory(Category.COUCH).stream().count(), 2, "ItemService.findAll() should find all items by categories!/Number of items in catalog is wrong!");
 		assertEquals(itemservice.findAllByCategory(Category.TABLE).stream().count(), 1, "ItemService.findAll() should find all items by categories!/Number of items in catalog is wrong!");
 		assertEquals(itemservice.findAllByCategory(Category.CHAIR).stream().count(), 1, "ItemService.findAll() should find all items by categories!/Number of items in catalog is wrong!");
 		assertEquals(itemservice.findAllByCategory(Category.SET).stream().count(), 1, "ItemService.findAll() should find all items by categories!/Number of items in catalog is wrong!");
