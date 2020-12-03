debbb18040e6caef759eb5b65e576bfa4fa5ae61
sjaster <sebastian.jaster@mailbox.tu-dresden.de>
2020-12-03 15:40:24 +0100

    add small integration for cart #48

--- src/test/java/furnitureshop/order/OrderControllerIntegrationTests.java
+++ src/test/java/furnitureshop/order/OrderControllerIntegrationTests.java
@@ -1,13 +1,22 @@
 package furnitureshop.order;
 
+import static org.hamcrest.CoreMatchers.endsWith;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import furnitureshop.admin.AdminController;
+import furnitureshop.FurnitureShop;
+import furnitureshop.inventory.Category;
+import furnitureshop.inventory.Item;
+import furnitureshop.inventory.Piece;
+import furnitureshop.supplier.Supplier;
+import org.javamoney.moneta.Money;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.salespointframework.core.Currencies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 /**
@@ -18,9 +27,20 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ContextConfiguration(classes = FurnitureShop.class)
 public class OrderControllerIntegrationTests {
 
 	@Autowired MockMvc mvc;
+
+	Supplier supplier;
+	Item item;
+
+	@BeforeEach
+	void setUp() {
+		supplier = new Supplier("supplier", 0.05);
+		item = new Piece(1, "Stuhl 1", Money.of(59.99, Currencies.EURO), "/resources/img/chair_2.jpg", "schwarz",
+				"Stuhl 1 in schwarz.", supplier, 5, Category.CHAIR);
+	}
 
 	/**
 	 * cartWebIntegrationTest method
@@ -37,4 +57,13 @@ public class OrderControllerIntegrationTests {
 			.andExpect(status().isOk())
 			.andExpect(view().name("cart"));
 	}
+
+	/*
+	@Test
+	void addItemIntegrationTest() throws Exception {
+		mvc.perform(post("/cart/{id}", item.getId())
+				.param("number", "5"))
+				.andExpect(status().is3xxRedirection())
+				.andExpect(header().string("Location", endsWith("/")));
+	}*/
 }
